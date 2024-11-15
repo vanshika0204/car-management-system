0f6b1fd0d108fc3816818de576b100d83c828b22
vanshikasehgal0204 <vanshikasehgal2902@gmail.com>
2024-11-15 23:30:45 +0530

    Car Application Commit.

---
+++ backend/index.js
@@ -0,0 +1,189 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection With MongoDB
+mongoose.connect("mongodb+srv://vanshika_sehgal:Vanshi1234@cluster0.8l2rd.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+
+// paste your mongoDB Connection string above with password
+// password should not contain '@' special character
+
+
+//Image Storage Engine 
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+const upload = multer({ storage: storage })
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `/images/${req.file.filename}`
+  })
+})
+
+
+// Route for Images folder
+app.use('/images', express.static('upload/images'));
+
+
+// MiddleWare to fetch user from token
+const fetchuser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, "secret_ecom");
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+};
+
+
+// Schema for creating user model
+const Users = mongoose.model("Users", {
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  date: { type: Date, default: Date.now() },
+});
+
+
+// Schema for creating Product
+const Product = mongoose.model("Product", {
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  car_type: { type: String, required: true },
+  company: { type: String, required: true },
+  dealer: { type: String, required: true }
+});
+
+
+// ROOT API Route For Testing
+app.get("/", (req, res) => {
+  res.send("Root");
+});
+
+
+// Create an endpoint at ip/login for login the user and giving auth-token
+app.post('/login', async (req, res) => {
+  console.log("Login");
+  let success = false;
+  let user = await Users.findOne({ email: req.body.email });
+  if (user) {
+    const passCompare = req.body.password === user.password;
+    if (passCompare) {
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      success = true;
+      console.log(user.id);
+      const token = jwt.sign(data, 'secret_ecom');
+      res.json({ success, token });
+    }
+    else {
+      return res.status(400).json({ success: success, errors: "please try with correct email/password" })
+    }
+  }
+  else {
+    return res.status(400).json({ success: success, errors: "please try with correct email/password" })
+  }
+})
+
+
+//Create an endpoint at ip/auth for regestring the user & sending auth-token
+app.post('/signup', async (req, res) => {
+  console.log("Sign Up");
+  let success = false;
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({ success: success, errors: "existing user found with this email" });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+  await user.save();
+  const data = {
+    user: {
+      id: user.id
+    }
+  }
+
+  const token = jwt.sign(data, 'secret_ecom');
+  success = true;
+  res.json({ success, token })
+})
+
+
+// endpoint for getting all products data
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products");
+  res.send(products);
+});
+
+// Create an endpoint for adding products using admin panel
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  }
+  else { id = 1; }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    description: req.body.description,
+    image: req.body.image,
+    car_type: req.body.car_type,
+    company: req.body.company,
+    dealer: req.body.dealer,
+  });
+  await product.save();
+  console.log("Saved");
+  res.json({ success: true, name: req.body.name })
+});
+
+
+// Create an endpoint for removing products using admin panel
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({ success: true, name: req.body.name })
+});
+
+// Starting Express Server
+app.listen(port, (error) => {
+  if (!error) console.log("Server Running on port " + port);
+  else console.log("Error : ", error);
+});
