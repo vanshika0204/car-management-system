97f92bdef8dec76d744920f64dee42bbc4dba90e
vanshika0204 <148622391+vanshika0204@users.noreply.github.com>
2024-11-17 21:23:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,16 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://car-management-system-tn39.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 // Database Connection With MongoDB
 mongoose.connect("mongodb+srv://vanshika_sehgal:Vanshi1234@cluster0.8l2rd.mongodb.net/",
